566d14c0e69e8958599008474074c2c00a3075e1
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-11-11 11:21:12 +0530

    feat: questions card

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
@@ -8,30 +9,30 @@ import Link from "next/link";
 
 const questions = [
   {
-    _id: 1,
+    _id: "1",
     title: "Cascading Deletes in SQLAlchemy?",
     tags: [
-      { _id: 1, name: "python" },
-      { _id: 2, name: "sql" },
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
     ],
-    author: "John Doe",
+    author: { _id: "3", name: "John Doe", picture: "profile.jpg" },
     upvotes: 10,
     views: 100,
-    answers: 2,
-    createdAt: "2023-11-11T12:00:00.000Z",
+    answers: [],
+    createdAt: new Date("2023-11-11T01:00:00.000Z"),
   },
   {
-    _id: 2,
+    _id: "2",
     title: "How to center a div?",
     tags: [
-      { _id: 1, name: "python" },
-      { _id: 2, name: "sql" },
+      { _id: "1", name: "css" },
+      { _id: "2", name: "html" },
     ],
-    author: "John Doe",
+    author: { _id: "4", name: "John Doe", picture: "profile.jpg" },
     upvotes: 10,
     views: 100,
-    answers: 2,
-    createdAt: "2023-11-11T12:00:00.000Z",
+    answers: [],
+    createdAt: new Date("2022-11-11T12:00:00.000Z"),
   },
 ];
 
@@ -63,7 +64,19 @@ export default function Home() {
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6">
         {questions.length > 0 ? (
-          questions.map((question) => "QuestionCard")
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There's no question to show"
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,41 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const currentDate = new Date();
+  const timeDifference = currentDate.getTime() - createdAt.getTime();
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  if (years > 0) {
+    return years === 1 ? "1 year ago" : `${years} years ago`;
+  } else if (months > 0) {
+    return months === 1 ? "1 month ago" : `${months} months ago`;
+  } else if (days > 0) {
+    return days === 1 ? "1 day ago" : `${days} days ago`;
+  } else if (hours > 0) {
+    return hours === 1 ? "1 hour ago" : `${hours} hours ago`;
+  } else if (minutes > 0) {
+    return minutes === 1 ? "1 minute ago" : `${minutes} minutes ago`;
+  } else {
+    return "Just now";
+  }
+};
+
+export const formatAndDivideNumber = (num: number): string => {
+  if (num >= 1e6) {
+    return (num / 1e6).toFixed(2) + "M";
+  } else if (num >= 1e3) {
+    return (num / 1e3).toFixed(2) + "K";
+  } else {
+    return num.toString();
+  }
+};

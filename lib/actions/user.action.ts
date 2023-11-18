8b47d10a6dba44157f69cf4fd3de4e23478d7705
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-11-18 19:50:29 +0530

    fix lint and fix radixui-dialog version

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -58,6 +58,7 @@ export async function deleteUser(params: DeleteUserParams) {
       throw new Error("User not found");
     }
 
+    // eslint-disable-next-line no-unused-vars
     const userQuestionIds = await Question.find({ author: user._id }).distinct(
       "_id"
     );
